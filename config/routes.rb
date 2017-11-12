b9c2335a6846a3fc0e815589107ef8307cc805e7
ultrasaber <saberthesaber@gmail.com>
2017-11-12 01:07:08 +0000

    Part 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'customers#index'
+  get 'customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized'
+  get 'customers/missing_email', to: 'customers#missing_email', as: 'missing_email'
 end
